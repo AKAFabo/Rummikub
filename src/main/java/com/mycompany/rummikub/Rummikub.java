166a27e03e4baf09bd76da092190ac2eda5e1c40
AKAFabo <faboherrera.fabo@gmail.com>
2023-10-30 00:46:16 -0600

    First Commit, main gui design

---
+++ src/main/java/com/mycompany/rummikub/Rummikub.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rummikub;
+
+import com.mycompany.rummikub.GUI.GameGui;
+
+/**
+ *
+ * @author Fabo
+ */
+public class Rummikub {
+
+    public static void main(String[] args) {
+        
+        GameGui gui = new GameGui();
+        gui.setVisible(true);
+        
+    }
+}
